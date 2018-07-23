1f54dfe86270a75e04175aec482c60095ee183af
Sebastian Jastrzebski <sebby2k@gmail.com>
2018-07-22 21:13:35 -0400

    project: relicense under GPLv3

--- src/app/logger.rs
+++ src/app/logger.rs
@@ -1,21 +1,6 @@
-/*
- * Copyright (c) 2016-2018 Sebastian Jastrzebski. All rights reserved.
- *
- * This file is part of zinc64.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// This file is part of zinc64.
+// Copyright (c) 2016-2018 Sebastian Jastrzebski. All rights reserved.
+// Licensed under the GPLv3. See LICENSE file in the project root for full license text.
 
 use std::collections::HashMap;
 use std::fs::File;
