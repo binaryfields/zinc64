bec0b7ba47cde90e80ba00c82026783a1a331221
Sebastian Jastrzebski <sebby2k@gmail.com>
2019-01-09 00:06:03 -0500

    raspi: initial code to boot under qemu

---
+++ zinc64-raspi/src/app.rs
@@ -0,0 +1,41 @@
+// This file is part of zinc64.
+// Copyright (c) 2016-2019 Sebastian Jastrzebski. All rights reserved.
+// Licensed under the GPLv3. See LICENSE file in the project root for full license text.
+
+use alloc::prelude::*;
+use alloc::rc::Rc;
+use alloc::sync::Arc;
+use zinc64_emu::system::{C64, C64Factory, Config};
+use zinc64_core::{new_shared, SoundOutput, SystemModel, VideoOutput};
+
+static RES_BASIC_ROM: &[u8] = include_bytes!("../../res/rom/basic.rom");
+static RES_CHARSET_ROM: &[u8] = include_bytes!("../../res/rom/characters.rom");
+static RES_KERNAL_ROM: &[u8] = include_bytes!("../../res/rom/kernal.rom");
+
+struct NullSound;
+impl SoundOutput for NullSound {
+    fn reset(&self) {}
+    fn write(&self, _samples: &[i16]) {}
+}
+
+struct NullVideo;
+impl VideoOutput for NullVideo {
+    fn get_dimension(&self) -> (usize, usize) {
+        (0, 0)
+    }
+    fn reset(&mut self) {}
+    fn write(&mut self, _index: usize, _color: u8) {}
+}
+
+pub fn run() {
+    let config = Rc::new(Config::new_with_roms(
+        SystemModel::from("pal"),
+        RES_BASIC_ROM,
+        RES_CHARSET_ROM,
+        RES_KERNAL_ROM,
+    ));    let factory = Box::new(C64Factory::new(config.clone()));
+    let video_output = new_shared(NullVideo {});
+    let sound_output = Arc::new(NullSound {});
+    let mut c64 = C64::build(config.clone(), &*factory, video_output, sound_output);
+    c64.reset(false);
+}
--- zinc64-raspi/src/main.rs
+++ zinc64-raspi/src/main.rs
@@ -2,46 +2,45 @@
 // Copyright (c) 2016-2019 Sebastian Jastrzebski. All rights reserved.
 // Licensed under the GPLv3. See LICENSE file in the project root for full license text.
 
+// https://docs.rust-embedded.org/book/collections/index.html
+// https://github.com/rust-embedded/cortex-m-rt/blob/master/examples/qemu.rs
+
 #![feature(alloc)]
+#![feature(alloc_error_handler)]
+#![no_main]
 #![no_std]
 
 extern crate alloc;
-extern crate log;
+extern crate panic_halt;
 
-use alloc::prelude::*;
-use alloc::rc::Rc;
-use alloc::sync::Arc;
-use zinc64_emu::system::{C64Factory, C64, Config};
-use zinc64_core::{new_shared, SoundOutput, SystemModel, VideoOutput};
+mod app;
 
-static RES_BASIC_ROM: &[u8] = include_bytes!("../../res/rom/basic.rom");
-static RES_CHARSET_ROM: &[u8] = include_bytes!("../../res/rom/characters.rom");
-static RES_KERNAL_ROM: &[u8] = include_bytes!("../../res/rom/kernal.rom");
+use alloc_cortex_m::CortexMHeap;
+use core::alloc::Layout;
+use cortex_m_rt ::entry;
+use cortex_m_semihosting::{debug, hprintln};
 
-struct NullSound;
-impl SoundOutput for NullSound {
-    fn reset(&self) {}
-    fn write(&self, _samples: &[i16]) {}
+const HEAP_SIZE: usize = 32 * 1024;
+
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+
+#[entry]
+fn main() -> ! {
+    // Initialize the allocator BEFORE you use it
+    unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
+
+    hprintln!("Starting emulator ...").unwrap();
+    //app::run();
+    debug::exit(debug::EXIT_SUCCESS);
+
+    loop {}
 }
 
-struct NullVideo;
-impl VideoOutput for NullVideo {
-    fn get_dimension(&self) -> (usize, usize) {
-        (0, 0)
-    }
-    fn reset(&mut self) {}
-    fn write(&mut self, _index: usize, _color: u8) {}
-}
-
-fn main() {
-    let config = Rc::new(Config::new_with_roms(
-        SystemModel::from("pal"),
-        RES_BASIC_ROM,
-        RES_CHARSET_ROM,
-        RES_KERNAL_ROM,
-    ));    let factory = Box::new(C64Factory::new(config.clone()));
-    let video_output = new_shared(NullVideo {});
-    let sound_output = Arc::new(NullSound {});
-    let mut c64 = C64::build(config.clone(), &*factory, video_output, sound_output);
-    c64.reset(false);
+#[alloc_error_handler]
+fn on_oom(_layout: Layout) -> ! {
+    //asm::bkpt();
+    hprintln!("ERROR: Out of memory!").unwrap();
+    debug::exit(debug::EXIT_FAILURE);
+    loop {}
 }
